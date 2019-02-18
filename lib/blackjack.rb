7af881ec2d1b921c9891f7cdcfb6ac08c739db6e
Ashley Archibald <aarchibald20@gmail.com>
2019-02-18 23:39:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,8 +25,10 @@ def get_user_input
   return gets.chomp
 end
 
-def end_game
+def end_game(number)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+
 end
 
 def initial_round
